b42d91269284b5a1116295a5e451cfaeead707dc
Draga Doncila Pop <ddon0001@student.monash.edu>
2021-07-16 19:23:42 +1000

    Handle multiscale data for ndvi layer and set layer scale

--- napari_interactive_sentinel/_function_widget.py
+++ napari_interactive_sentinel/_function_widget.py
@@ -1,5 +1,5 @@
-import warnings
-from napari.types import ImageData, LayerDataTuple
+from napari.types import LayerDataTuple
+from napari.layers import Image
 from napari_plugin_engine import napari_hook_implementation
 from ._utils import compute_ndvi_layer
 
@@ -9,10 +9,15 @@ def napari_experimental_provide_function():
     return get_ndvi_layer
 
 
-def get_ndvi_layer(red: ImageData, nir: ImageData) -> LayerDataTuple:
-    ndvi_levels = compute_ndvi_layer(nir, red)
+def get_ndvi_layer(red: Image, nir: Image) -> LayerDataTuple:
+    ndvi_levels = compute_ndvi_layer(nir.data, red.data)
     return (
         ndvi_levels,
-        {"name": "NDVI", "colormap": "RdYlGn", "contrast_limits": (0, 1)},
+        {
+            "name": "NDVI", 
+            "colormap": "RdYlGn", 
+            "contrast_limits": (0, 1),
+            "scale": red.scale
+        },
         "image",
     )
